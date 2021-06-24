a386b3d9a8a14bd3a44d202118dc8714cf8a740a
eduardo0915 <eduardomartinez0215@hotmail.com>
2021-06-23 23:16:21 -0500

    Actualizacion Bienvenidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,8 +21,13 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./paginas/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'bienvenida',
+    loadChildren: () => import('./paginas/bienvenida/bienvenida.module').then( m => m.BienvenidaPageModule)
   }
+  
 ];
 
 @NgModule({
