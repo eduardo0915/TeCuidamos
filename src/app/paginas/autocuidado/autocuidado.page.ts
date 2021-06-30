075b327056694a2b819fd8c68a067abd18f776ad
eduardo0915 <39531873+eduardo0915@users.noreply.github.com>
2021-06-30 03:33:02 -0400

    recordatorios
    recordatorios y autocuidado

---
+++ src/app/paginas/autocuidado/autocuidado.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autocuidado',
+  templateUrl: './autocuidado.page.html',
+  styleUrls: ['./autocuidado.page.scss'],
+})
+export class AutocuidadoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
