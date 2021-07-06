cc6c042afa7a1d05a18834d4de9349e75b598281
eduardo0915 <eduardomartinez0215@hotmail.com>
2021-07-05 22:59:48 -0500

    app.routing Resuelto 05/07/21

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-<<<<<<< Updated upstream
 import { AuthGuard } from './auth.guard';
 =======
 
